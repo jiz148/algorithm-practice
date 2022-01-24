0d45cc77fbb4506ff8641eef07c8e11ff35f3a1b
jiz148 <jizjiz148148@gmail.com>
2022-01-24 17:26:34 -0500

    modified merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,8 +1,13 @@
 def merge(array, start, middle, end):
+    """
+    Merge two sorted lists,
+    counts and returns the inversions within two lists as well
+    """
+    count = 0
 
     # make sub arrays
-    sub1 = array[start:middle + 1]
-    sub2 = array[middle + 1:end + 1]
+    sub1 = array[start:middle + 1].copy()
+    sub2 = array[middle + 1:end + 1].copy()
 
     # give infinity at each end
     sub1.append(float('inf'))
@@ -15,26 +20,54 @@ def merge(array, start, middle, end):
         if sub1[i] <= sub2[j]:
             array[k] = sub1[i]
             i += 1
+            count += j
         else:
             array[k] = sub2[j]
             j += 1
 
+    return count
+
+
 def merge_sort(array, start, end):
+    """
+    Sorts the array and returns the number of inversions
+    """
 
     if end > start:
         # divide
         middle = int((start + end) / 2)
 
         # conquer
-        merge_sort(array, start, middle)
-        merge_sort(array, middle + 1, end)
+        a = merge_sort(array, start, middle)
+        b = merge_sort(array, middle + 1, end)
 
         # combine
-        merge(array, start, middle, end)
+        count = merge(array, start, middle, end)
+
+        return a + b + count
+    else:
+        return 0
+
+
+def test_number_of_inversions(array):
+    """
+    @return: the number of inversions in an array
+    """
+    array = array.copy()
+    count = 0
+    while len(array) > 0:
+        item = array.pop(0)
+        for another_item in array:
+            if another_item < item:
+                count += 1
+
+    return count
 
 
 if __name__ == '__main__':
     array = [9, 33, 3, 9, 2, 4, 2, 3, 1, 5, 4]
-    merge_sort(array, 0, len(array) - 1)
-
+    actual_count = test_number_of_inversions(array)
+    count = merge_sort(array, 0, len(array) - 1)
+    print("actual count:", actual_count)
+    print("result count:", count)
     print(array)
